383b6ccdf9428ada7d3b7b45652739ce506f7f91
doantoni <65489913+doantoni@users.noreply.github.com>
2021-05-01 16:25:51 +0200

    updated version number

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div className="about">
-      <h4>Version 1.0.0.</h4>
+      <h4>Version 1.0.1.</h4>
       <Link to="/react-task-tracker-frontend-app">Go Back</Link>
     </div>
   );
